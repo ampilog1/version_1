46a472f8c40da5899791a7d5c40c9a8fcbe6ffc9
ampilog1 <ampilog1@gmail.com>
2023-04-19 13:03:19 +0300

    Добавил две модели

--- src/parsing_app/models.py
+++ src/parsing_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='город')
-    slug = models.SlugField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='город', unique=True)
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'город'
@@ -11,4 +11,36 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык програмирования', unique=True)
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык програмирования'
+        verbose_name_plural = 'Языки програмирования'
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='Город', related_name='vacancies')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Язык программирования')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return self.title
 
