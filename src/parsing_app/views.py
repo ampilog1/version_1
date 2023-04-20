e602ea83cc1ba352d4cedc1d023b0327c2c797b4
ampilog1 <ampilog1@gmail.com>
2023-04-20 16:08:23 +0300

    добавлен Шаблон, файл forms.py и одна функция для отображения

--- src/parsing_app/views.py
+++ src/parsing_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, \
+    DeleteView
+
+from .forms import FindForm, VForm
+from .models import Vacancy
+
+
+def home_view(request):
+    # print(request.GET)
+    form = FindForm()
+
+    return render(request, 'scraping/home.html', {'form': form})
 
 # Create your views here.
