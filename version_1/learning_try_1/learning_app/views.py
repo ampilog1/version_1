42bdcdf721a5edb59ba3052c1d9996b0decf726b
ampilog1 <ampilog1@gmail.com>
2023-10-09 13:41:35 +0300

    Присоединил форму ввода к парсеру

--- version_1/learning_try_1/learning_app/views.py
+++ version_1/learning_try_1/learning_app/views.py
@@ -7,26 +7,30 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    # print(request.GET)
     if request.method == 'POST':
-        form = FindForm()
+        # print(request.POST)
+        form = FindForm(request.POST)
         if form.is_valid():
             data_for_find = form.cleaned_data.get('vacancy_find')
+            a = 1
 
             parsers = (hh, superjob, zarplata)
             vacancy = []
             for pars in parsers:
                 job = pars(data_for_find)
                 vacancy += job
+            b = 2
             date = datetime.datetime.now().date()
             name = 'Dave'
             context = {'date': date, 'name': name, 'vacancy': vacancy, 'form': form}
             return render(request, 'learning_app/home.html', context)
 
-    else:
-        form = FindForm()
-        date = datetime.datetime.now().date()
-        name = 'Dave'
-        context = {'date': date, 'name': name,  'form': form}
+    form = FindForm()
+    date = datetime.datetime.now().date()
+    name = 'Dave'
+    context = {'date': date, 'name': name, 'form': form}
+
     return render(request, 'learning_app/home.html', context)
 
 
