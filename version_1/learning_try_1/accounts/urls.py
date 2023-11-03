5d3059b933d63f9ce1a9b291d7e938a915a170e6
ampilog1 <ampilog1@gmail.com>
2023-11-03 12:26:27 +0300

    добавил добавил выход

--- version_1/learning_try_1/accounts/urls.py
+++ version_1/learning_try_1/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', views.UserCreateView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
