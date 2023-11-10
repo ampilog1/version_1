f5a20108db4b24b1db2457aba7f93a3aee5a570f
ampilog1 <ampilog1@gmail.com>
2023-11-10 12:19:45 +0300

    Работа над ошибками

--- version_1/learning_try_1/learning_app/views.py
+++ version_1/learning_try_1/learning_app/views.py
@@ -44,14 +44,12 @@ def list_view(request):
         form = FindForm(request.POST)
         if form.is_valid():
             data_for_find = form.cleaned_data.get('vacancy_find')
-            a = 1
 
             parsers = (hh, superjob, zarplata)
             vacancy = []
             for pars in parsers:
                 job = pars(data_for_find)
                 vacancy += job
-            b = 2
             for vac in vacancy:
                 v = Vacancy(**vac)
                 v.save()
