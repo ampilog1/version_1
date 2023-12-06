76f0b9d9c61979a2bdc8d6984bfea0439aaef4c6
ampilog1 <ampilog1@gmail.com>
2023-12-06 13:27:05 +0300

    Создание телеграм бота

--- version_1/learning_try_1/learning_app/management/commands/bot.py
+++ version_1/learning_try_1/learning_app/management/commands/bot.py
@@ -1,8 +1,19 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from telebot import TeleBot
+from learning_app.models import *
+from django.forms.models import model_to_dict
 
 bot = TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
+
+
+@bot.message_handler(commands=['huy'])
+def parse_site(message):
+    text = message.text.split()[1]
+    chat_id = message.chat.id
+    vacancy_all = model_to_dict(Vacancy.objects.all())
+    for vacancy_send in vacancy_all[:20]:
+        bot.send_message(chat_id, f'{key} - {vacancy_send[key]}')
 
 
 class Command(BaseCommand):
