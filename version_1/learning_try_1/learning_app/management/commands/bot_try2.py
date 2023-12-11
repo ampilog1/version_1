271403337eb228e08b9d00f3250d45e02afaf7a4
ampilog1 <ampilog1@gmail.com>
2023-12-11 13:09:50 +0300

    Создание телеграм бота

--- version_1/learning_try_1/learning_app/management/commands/bot_try2.py
+++ version_1/learning_try_1/learning_app/management/commands/bot_try2.py
@@ -1,8 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 import telebot
-from learning_app.models import *
-
 
 bot = telebot.TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
 
@@ -10,18 +8,6 @@ bot = telebot.TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
-
-
-@bot.message_handler(commands=['hay'])
-def first_vacancy_20(message):
-    chat_id = message.chat.id
-    vacancy_all = Vacancy.objects.values()
-    for vacancy_prep in vacancy_all[:20]:
-        vacancy_prep_vision = vacancy_prep.values()
-        for value in vacancy_prep_vision:
-            if value is not None:
-                # vacancy_send = [f'{value}' for value in vacancy_prep.values()]
-                bot.send_message(chat_id, value)
 
 
 class Command(BaseCommand):
