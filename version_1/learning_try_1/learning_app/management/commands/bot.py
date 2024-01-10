76feb2adbb97c2c1b7c83707287718699fe4e888
ampilog1 <ampilog1@gmail.com>
2024-01-10 13:40:55 +0300

    пробую переделать телеграмбота для одновременного запуска с сайтом

--- version_1/learning_try_1/learning_app/management/commands/bot.py
+++ version_1/learning_try_1/learning_app/management/commands/bot.py
@@ -1,19 +1,20 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
-import telebot
+from telebot.async_telebot import AsyncTeleBot
 from learning_app.models import *
+import asyncio
 
 
-bot = telebot.TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
+bot = AsyncTeleBot(settings.TELEGRAM_BOT_API_KEY)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+async def send_welcome(message):
+    await bot.reply_to(message, "Howdy, how are you doing?")
 
 
-@bot.message_handler(commands=['hay'])
-def first_vacancy_20(message):
+@bot.message_handler(func=lambda message: True)
+async def first_vacancy_20(message):
     chat_id = message.chat.id
     vacancy_all = Vacancy.objects.values()
     for vacancy_prep in vacancy_all[:20]:
@@ -22,13 +23,11 @@ def first_vacancy_20(message):
         # for value in vacancy_prep_vision:
         #     if value is not None:
         # vacancy_send = [f'{value}' for value in vacancy_prep.values()]
-        bot.send_message(chat_id, vacancy_send)
+        await bot.send_message(chat_id, vacancy_send)
 
 
 class Command(BaseCommand):
     help = 'Implemented to Django application telegram bot setup command'
 
     def handle(self, *args, **options):
-        bot.enable_save_next_step_handlers(delay=2)
-        bot.load_next_step_handlers()
-        bot.infinity_polling()
+        asyncio.run(bot.polling())
